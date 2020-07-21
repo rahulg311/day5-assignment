750674c175fd33a1fc31abd8cefbc8cbef6761ba
rahul kumar kasaudhon <68266050+rahulg311@users.noreply.github.com>
2020-07-21 14:32:47 +0530

    Add files via upload

---
+++ day5 assignment/question1.js
@@ -0,0 +1,22 @@
+
+// var num = parseInt(prompt('Enter a number please'));
+// var numArr=[];
+// for (var i = 1; i <= num+1; i++) {
+//     if(i<num+1){
+//       console.log(i);
+//       numArr.push(i);
+//     }
+// }
+// console.log(numArr);
+// const oldNoArrayCubes=numArr.filter(function(item){
+//   if(item%2!=0){
+//     return item*item*item;
+//   }
+// });
+
+// console.log(oldNoArrayCubes)
+
+// Question 2
+
+class user(){
+  
